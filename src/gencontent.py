221ff1f2b6ddab32660c7141ba7f8ed5b75495fb
Aidan Nowakowski <aidannowakowski@gmail.com>
2026-05-12 00:04:39 -0400

    attempt to fix link issue

--- src/gencontent.py
+++ src/gencontent.py
@@ -30,6 +30,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    if basepath != "/":
+        basepath = "/" + basepath.strip("/") + "/"
     template = template.replace('href="/', 'href="' + basepath)
     template = template.replace('src="/', 'src="' + basepath)
 
